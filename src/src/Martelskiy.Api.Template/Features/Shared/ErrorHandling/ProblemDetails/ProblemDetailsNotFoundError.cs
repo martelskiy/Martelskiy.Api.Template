aeb19d3245af1a8c33e66e54d3a33a975c0a9385
oleg <oleg.iaremchuk@protonmail.com>
2022-05-08 18:08:34 +0200

    update packages, remove some unused stuff

--- src/src/Martelskiy.Api.Template/Features/Shared/ErrorHandling/ProblemDetails/ProblemDetailsNotFoundError.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Martelskiy.Api.Template.Features.Shared.ErrorHandling.ProblemDetails
-{
-    public class ProblemDetailsNotFoundError : ProblemDetailsError
-    {
-        public ProblemDetailsNotFoundError(string detail) : base(ProblemDetailsErrorType.NotFound, detail)
-        {
-        }
-    }
-}
